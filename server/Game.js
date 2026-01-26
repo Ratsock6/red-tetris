6ff7a2acf3cac3c7484edc04bb8657388f7fbf98
tuvosyl <valentin.soltys@gmail.com>
2026-01-26 19:05:39 +0100

    oui

--- server/Game.js
+++ server/Game.js
@@ -1,16 +1,38 @@
 const ROWS = 20;
 const COLS = 10;
 class Game {
-	constructor() {
+	constructor(blockList = [], blockIndex = 0) {
 		this.board = Array.from({ length: ROWS }, () => Array(COLS).fill(0));
 		this.active = true;
-		this.current_block = 0
+		this.blockList = blockList;
+		this.blockIndex = blockIndex;
 
+		// Place initial block if blockList is provided
+		if (blockList.length > 0) {
+			const block = blockList[blockIndex];
+			const initialPosition = { x: 3, y: 0 };
+			this.placeBlock(block, initialPosition);
+		}
 	}
 	updateBoard(cells) {
 		for (let r = 0; r < ROWS; r++) {
 			for (let c = 0; c < COLS; c++) {
 				this.board[r][c] = cells[r][c];
+			}
+		}
+	}
+	placeBlock(block, position) {
+		const { shape, id } = block;
+		const { x, y } = position;
+		for (let r = 0; r < shape.length; r++) {
+			for (let c = 0; c < shape[r].length; c++) {
+				if (shape[r][c]) {
+					const boardRow = y + r;
+					const boardCol = x + c;
+					if (boardRow >= 0 && boardRow < ROWS && boardCol >= 0 && boardCol < COLS) {
+						this.board[boardRow][boardCol] = id;
+					}
+				}
 			}
 		}
 	}
--- server/Player.js
+++ server/Player.js
@@ -1,15 +1,17 @@
 
 import Game from './Game.js';
 class Player {
-	constructor(id, name) {
+	constructor(id, name, room = null) {
 		this.id = id;
 		this.name = name;
 		this.score = 0;
-		this.game = new Game();
+		this.room = room;
+		this.blockIndex = 0;
+		this.game = new Game(room ? room.blocklist : [], this.blockIndex);
 	}
 
 	newGame() {
-		this.game = new Game();
+		this.game = new Game(this.room ? this.room.blocklist : [], this.blockIndex);
 		this.score = 0;
 
 	}
